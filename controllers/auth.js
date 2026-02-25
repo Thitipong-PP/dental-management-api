20df1cd37f545289d0361d26ffad699423ca855f
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-26 00:07:51 +0700

    Make login in auth controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,3 +33,44 @@ exports.register = async (req, res, next) => {
         console.error(err.message);
     }
 };
+
+// @desc    Login
+// @route   POST /auth/login
+// @access  Public
+exports.login = async (req, res, next) => {
+    // Get body request
+    const {email, password} = req.body;
+
+    // Validate email & password
+    if (!email || !password) return res.status(400).json({
+        success: false,
+        message: "Please provide an email and password"
+    });
+
+    // Find user in database
+    const user = await User.findOne({email}).select('+password');
+
+    // Don't find user in database
+    if (!user) return res.status(400).json({
+        success: false,
+        message: "Invalid credentials"
+    });
+
+    // Check matched password
+    const isMatch = await user.matchPassword(password);
+
+    // Password don't match
+    if (!isMatch) return res.status(401).json({
+        success: false,
+        message: "Invalid password"
+    });
+
+    // Create token for JWT
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({
+        success: true,
+        data: user,
+        token
+    });
+};
